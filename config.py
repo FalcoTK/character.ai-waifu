ad756fa12be1e2280f8bea6c31bceab17923b6e2
Tokai Falco <160510985+FalcoTK@users.noreply.github.com>
2024-02-28 06:21:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 Owner_ID="-"
 bot_id_chat1="-"
-bot_id_chat2=""
+bot_id_chat2="" #Dont Fill it :D
 
-chat2=False
-chat1=True #Dont Enable it :D
+chat2=False #Dont Enable it :D
+chat1=True 
 
 translate=True
 target_lang="id"
